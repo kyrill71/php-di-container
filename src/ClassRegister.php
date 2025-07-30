fe7c6db4f3426cc6c3d9c53fee36d6dd1dc5760e
Kyrill van Fessem <kyrill@kvf-development.nl>
2025-07-30 09:58:45 +0200

    style: fixed spacing issues

--- src/ClassRegister.php
+++ src/ClassRegister.php
@@ -1,12 +1,18 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Kyrill\PhpDiContainer;
 
+use Kyrill\PhpDiContainer\Exception\ClassHasNotBeenRegisteredException;
+
 class ClassRegister
 {
-
-    public function registerFile(string $path, FileReaderInterface $fileReader, Container $container): void{
+    /**
+     * @throws ClassHasNotBeenRegisteredException
+     */
+    public function registerFile(string $path, FileReaderInterface $fileReader, Container $container): void
+    {
         $content = $fileReader->read($path);
         $FileToDefinition = new ArrayToDefinition();
         $classDefinitions = $FileToDefinition->convert($content);
